fd8f35733b338c2bc79de057443d1efe1d22a50d
yannick-lam <y.lam@gmx.at>
2022-11-19 07:27:28 +0100

    new site
    first upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function show() {
+    document.querySelector(".hamburger").classList.toggle("open")
+    document.querySelector(".navigation").classList.toggle("active")
+}
+
+// var counter = 1;
+// setInterval(function(){
+//   document.getElementById("radio" + counter).checked = true;
+//   counter++;
+//   if(counter > 4){
+//     counter = 1;
+//   }
+// }, 3000);
