187e700c846c92a0c8e82c1b716cc26285be664f
yannick-lam <y.lam@gmx.at>
2022-11-23 10:47:13 +0100

    h
    h

--- script.js
+++ script.js
@@ -1,16 +1,72 @@
-const nextBtn = document.getElementById("next-btn")
-const formEl = document.getElementById("form-element")
-
-
+const nextBtn = document.querySelector(".next-btn")
+const backBtn = document.querySelector(".back-btn")
+const formEl = document.querySelector(".form-section-two-container")
 
 nextBtn.addEventListener("click", function() {
-    
+    formEl.style.left = "-100%";
+    // formEl.style.bottom = "-10%";
+})
+
+backBtn.addEventListener("click", function() {
+    formEl.style.left = "0%";
 })
 
 function show() {
     document.querySelector(".hamburger").classList.toggle("open")
     document.querySelector(".navigation").classList.toggle("active")
 }
+
+
+
+
+
+function sendMail() {
+    const sex = document.getElementById("sex").value
+    const firstname = document.getElementById("fname").value
+    const lastname = document.getElementById("lname").value
+    const email = document.getElementById("email").value
+    const telnumber = document.getElementById("telnumber").value
+    const street = document.getElementById("street").value
+    const city = document.getElementById("location").value
+    const locnumber = document.getElementById("locnumber").value
+    const country = document.getElementById("country").value
+    const fax = document.getElementById("fax").value
+    var params = {
+        firstname: document.getElementById("fname").value,
+        lastname: document.getElementById("lname").value,
+        message: `
+            Anrede: ${sex}
+            Vorname: ${firstname}
+            Nachname: ${lastname}
+            E-Mail: ${email}
+            Telefon: ${telnumber}
+            Straße: ${street}
+            Ort: ${city}
+            PLZ: ${locnumber}
+            Land: ${country}
+            Fax: ${fax}
+        `
+    }
+    const serviceId = "service_jjgus4m"
+    const templateId = "template_25fsecj"
+    
+    emailjs
+    .send(serviceId, templateId, params)
+    .then(response => {
+        console.log(response)
+        alert("Deine Nachricht wurde erfolgreich gesendet")
+    })
+    .catch(err => {
+        console.log("Error")
+    })
+}
+
+
+
+
+
+
+
 
 // var counter = 1;
 // setInterval(function(){
